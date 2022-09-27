3772f1ee1df6c0396e3c23cf50c8813d567ccd07
ropoko <rodrigostramantinoli@gmail.com>
2022-09-27 01:33:42 -0300

    feat: wip voice feature

--- index.ts
+++ index.ts
@@ -1,15 +1,27 @@
-import { Client, GatewayIntentBits } from "discord.js";
+import {
+  joinVoiceChannel,
+  entersState,
+  VoiceConnectionStatus,
+  createAudioPlayer,
+  NoSubscriberBehavior,
+  createAudioResource
+} from '@discordjs/voice'
+import { Client, IntentsBitField } from "discord.js";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [IntentsBitField.Flags.GuildMessages, IntentsBitField.Flags.GuildVoiceStates] });
 
 client.on("ready", () => {
   console.log("All right man! What do you need, an?");
 });
 
 client.on("messageCreate", (msg) => {
+  if (msg.author.bot) return;
+
+  if (!msg.content.startsWith(process.env.PREFIX!)) return;
+
   if (msg.content === "hello") {
     msg.reply({
       content: "world",
