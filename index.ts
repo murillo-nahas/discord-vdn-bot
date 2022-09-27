f5a7d5db778cb6cb0e317f21f7e5fab9f1ccb18e
murillo.nahas <murillonahas@hotmail.com>
2022-09-26 22:14:48 -0300

    feat: setup bot

--- index.ts
+++ index.ts
@@ -1,1 +1,20 @@
-const discord = require("discord.js");
+import { Client, GatewayIntentBits } from "discord.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on("ready", () => {
+  console.log("All right man! What do you need, an?");
+});
+
+client.on("messageCreate", (msg) => {
+  if (msg.content === "hello") {
+    msg.reply({
+      content: "world",
+    });
+  }
+});
+
+client.login(process.env.TOKEN).catch((err: any) => console.log(err));
