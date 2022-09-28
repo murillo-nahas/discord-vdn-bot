0a40397c62e7d4cfc1084d6aba2e0aa462cf6107
ropoko <rodrigostramantinoli@gmail.com>
2022-09-28 11:22:51 -0300

    chore: updated discordjs to v14 and its working

--- src/index.ts
+++ src/index.ts
@@ -6,28 +6,31 @@ import {
   NoSubscriberBehavior,
   createAudioResource
 } from '@discordjs/voice'
-import { Client, Intents } from 'discord.js'
+import { Client, IntentsBitField, GatewayIntentBits } from 'discord.js'
 import dotenv from 'dotenv';
 
 import { VoiceCommands } from './commands/voiceCommands';
 
 dotenv.config();
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES] });
+const client = new Client({ intents: [
+	IntentsBitField.Flags.GuildMessages,
+	IntentsBitField.Flags.MessageContent,
+	GatewayIntentBits.Guilds,
+	GatewayIntentBits.GuildMessages,
+	GatewayIntentBits.MessageContent
+] });
 
 client.on('ready', () => {
   console.log('All right man! What do you need, an?');
 });
 
-client.on('messageCreate', (msg: any) => {
-	console.log(msg);
+client.on('messageCreate', msg => {
   if (msg.author.bot) return;
 
   if (!msg.content.startsWith(process.env.PREFIX!)) return;
 
 	const { channel } = msg.member!.voice;
-
-	console.log(msg.member);
 
 	if (!channel) {
 		msg.channel.send('Você deve estar em um canal de voz!');
